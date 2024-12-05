d7c0a1b34966e7a6d638841c8e072a354cf3ad08
Ahad-Miah <ahadahmedcc@gmail.com>
2024-12-05 19:17:21 +0600

    added sort button functionality on All equipments page

--- src/Pages/AllSportsEquipment.jsx
+++ src/Pages/AllSportsEquipment.jsx
@@ -1,42 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 
 const AllSportsEquipment = () => {
     const allData = useLoaderData();
+    const[allEquipments,setAllEquipments]=useState(allData);
+
+    const handleSort = () => {
+        const sortedEquipments = [...allEquipments].sort((a, b) => a.price - b.price); 
+        setAllEquipments(sortedEquipments); 
+      };
     return (
-        <div className="overflow-x-auto">
-            <table className="table table-zebra w-full">
-                {/* head */}
-                <thead>
-                    <tr>
-                        <th className="p-2">#</th>
-                        <th className="p-2">Name</th>
-                        <th className="p-2">Category</th>
-                        <th className="p-2">Price</th>
-                        <th className="p-2">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        allData?.map((data, idx) => (
-                            <tr key={data._id} className="text-center">
-                                <td className="p-2">{idx + 1}</td>
-                                <td className="p-2">{data.name}</td>
-                                <td className="p-2">{data.category}</td>
-                                <td className="p-2">{data.price}</td>
-                                <td className="p-2">
-                                    <Link to={`/details/${data._id}`}>
-                                        <div className="badge 
+        <div>
+           <div className='flex justify-center'>
+           <button onClick={handleSort} className='btn btn-primary'>Sort by price</button>
+           </div>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    <thead>
+                        <tr className='text-center'>
+                            <th className="p-2">#</th>
+                            <th className="p-2">Name</th>
+                            <th className="p-2">Category</th>
+                            <th className="p-2">Price</th>
+                            <th className="p-2">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            allEquipments?.map((data, idx) => (
+                                <tr key={data._id} className="text-center">
+                                    <td className="p-2">{idx + 1}</td>
+                                    <td className="p-2">{data.name}</td>
+                                    <td className="p-2">{data.category}</td>
+                                    <td className="p-2">{data.price}</td>
+                                    <td className="p-2">
+                                        <Link to={`/details/${data._id}`}>
+                                            <div className="badge 
                                         text-[18px]
                                         md:font-semibold badge-secondary cursor-pointer"><span className='hidden md:block mr-2'>View</span>Details</div>
-                                    </Link>
-                                </td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+                                        </Link>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
+
 
     );
 };
